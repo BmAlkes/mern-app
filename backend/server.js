249a871a020ab868d6fa2869eaae103e6c222914
bmalkes <bmalkes@gmail.com>
2022-12-09 20:58:25 +0200

    feat:add connect with DB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/connectDB");
 
 const app = express();
+connectDB();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
 const PORT = process.env.PORT || "4000";
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
