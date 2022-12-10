407078f503449cc5bb4f740577a47359a5ab1d48
bmalkes <bmalkes@gmail.com>
2022-12-10 12:52:13 +0200

    feat:update route

--- backend/Controllers/taskController.js
+++ backend/Controllers/taskController.js
@@ -17,7 +17,7 @@ const getTasks = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
-
+// get a single task
 const getTask = async (req, res) => {
   try {
     const { id } = req.params;
@@ -31,6 +31,7 @@ const getTask = async (req, res) => {
   }
 };
 
+// delete Task
 const deleteTask = async (req, res) => {
   try {
     const { id } = req.params;
@@ -40,5 +41,21 @@ const deleteTask = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
-const updateTask = async (req, res) => {};
+// Update Task
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json(`NO task with id: ${id}`);
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 module.exports = { createTask, getTasks, getTask, deleteTask, updateTask };
