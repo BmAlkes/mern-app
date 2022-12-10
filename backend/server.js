e197b43ef36ebcae99ae23623fa14777b4efa807
bmalkes <bmalkes@gmail.com>
2022-12-10 08:47:21 +0200

    feat:add routes get & post

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,41 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
+const Task = require("./model/taskModel");
 
 const app = express();
 connectDB();
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// function looger(req, res, next) {
+//   console.log("middleware running");
+//   console.log(req.method);
+//   next();
+// }
+
 // Routes
 app.get("/", (req, res) => {
   res.send("Home Page");
+});
+app.post("/api/tasks", async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+// Get / Read Data
+app.get("/api/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
 });
 
 const PORT = process.env.PORT || "4000";
