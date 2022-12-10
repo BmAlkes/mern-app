9d35a8ac41850fc409684aba4bc9b4d207f01567
bmalkes <bmalkes@gmail.com>
2022-12-10 12:27:14 +0200

    feat:delete route

--- backend/Controllers/taskController.js
+++ backend/Controllers/taskController.js
@@ -22,9 +22,23 @@ const getTask = async (req, res) => {
   try {
     const { id } = req.params;
     const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json(`No task with id ${id}`);
+    }
     res.status(200).json(task);
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
 };
-module.exports = { createTask, getTasks, getTask };
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTask = await Task.findByIdAndDelete(id);
+    return res.status(200).send({ msg: `task deleted` });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+const updateTask = async (req, res) => {};
+module.exports = { createTask, getTasks, getTask, deleteTask, updateTask };
--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -3,6 +3,8 @@ const {
   createTask,
   getTasks,
   getTask,
+  deleteTask,
+  updateTask,
 } = require("../Controllers/taskController");
 
 const router = express.Router();
@@ -10,5 +12,7 @@ const router = express.Router();
 router.post("/api/tasks", createTask);
 router.get("/api/tasks", getTasks);
 router.get("/api/tasks/:id", getTask);
+router.delete("/api/tasks/:id", deleteTask);
+router.put("/api/tasks/:id", updateTask);
 
 module.exports = router;
