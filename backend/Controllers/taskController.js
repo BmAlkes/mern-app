ba0a86073864d07bdbfd2df9da4967bc957fa836
bmalkes <bmalkes@gmail.com>
2022-12-10 12:07:47 +0200

    feat:get task by id

--- backend/Controllers/taskController.js
+++ backend/Controllers/taskController.js
@@ -1,4 +1,4 @@
-const Task = require("../model/taskModel");
+const Task = require("../models/taskModel");
 
 const createTask = async (req, res) => {
   try {
@@ -18,4 +18,13 @@ const getTasks = async (req, res) => {
   }
 };
 
-module.exports = { createTask, getTasks };
+const getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+module.exports = { createTask, getTasks, getTask };
--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const Task = require("../model/taskModel");
-const { createTask, getTasks } = require("../Controllers/taskController");
+const {
+  createTask,
+  getTasks,
+  getTask,
+} = require("../Controllers/taskController");
 
 const router = express.Router();
 
 router.post("/api/tasks", createTask);
-
 router.get("/api/tasks", getTasks);
+router.get("/api/tasks/:id", getTask);
+
 module.exports = router;
